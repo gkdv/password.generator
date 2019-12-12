2ce2f9eb8e0a1096920fbe4db3685fa7938c0904
garrett krage <garrett.krage@gmail.com>
2019-12-11 19:12:50 -0700

    added light grey background to body and made generate password button red

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var header = document.getElementsByTagName('h1');
 header[0].align = 'center';
 
+document.body.style.backgroundColor = 'lightgrey';
+// document.getElementsByClassName("wrapper").style.backgroundColor = 'white';
+// document.getElementsByClassName("wrapper").style.margin = '5%';
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.style.backgroundColor = 'red';
+generateBtn.style.color = 'white';
+generateBtn.style.borderRadius = '3px';
 
 
 // Write password to the #password input
