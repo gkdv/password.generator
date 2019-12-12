f6d3b53b367511a73d7699cd1c233c3658afb909
garrett krage <garrett.krage@gmail.com>
2019-12-11 20:07:28 -0700

    added arrays of prompt values

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ document.body.style.backgroundColor = 'lightgrey';
 // document.getElementsByClassName("wrapper").style.margin = '5%';
 
 var characterCount = prompt("How many characters would you like your password to be? \nMust be between 8 and 128 characters.");
-console.log(characterCount);
+// console.log(characterCount);
 var specialCharacters = prompt('would you like to include special characters? \nYes or No');
-console.log(specialCharacters);
+// console.log(specialCharacters);
 var numericCharacter = prompt('Would you like to include numeric characters? \nYes or No');
-console.log(numericCharacter);
+// console.log(numericCharacter);
 var lowerCase = prompt('Would you like to include lower case letter? \nYes or No');
-console.log(lowerCase);
+// console.log(lowerCase);
 var upperCase = prompt('would you like to include upper case letters? \nYes or No');
-console.log(upperCase);
+// console.log(upperCase);
+
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArray = ['Z','X','C','V','B','N','M','L','K','J','H','G','F','D','S','A','Q','W','E','R','T','Y','U','I','O','P'];
+var numericArray = [1,2,3,4,5,6,7,8,9,0];
+var specialCharacterArray = ['?','!',',','.','<','>',';',':','/','#','@','-','_'];
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
