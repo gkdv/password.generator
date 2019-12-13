c6c970573187322b9819472cc95b36d387404378
garrett krage <garrett.krage@gmail.com>
2019-12-12 21:18:46 -0700

    css styling is complete except for the size of the password box. starting to work on conditionals for prompts

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-// var header = document.getElementsByTagName('h1');
-// header[0].align = 'center';
-
-document.body.style.backgroundColor = 'lightgrey';
-// document.getElementsByClassName("wrapper").style.backgroundColor = 'white';
-// document.getElementsByClassName("wrapper").style.margin = '5%';
+// a little document styling
+// document.body.style.backgroundColor = 'lightgrey';
 
 
-// var characterCount = prompt("How many characters would you like your password to be? \nMust be between 8 and 128 characters.");
-// console.log(characterCount);
-// var specialCharacters = prompt('would you like to include special characters? \nYes or No');
-// console.log(specialCharacters);
-// var numericCharacter = prompt('Would you like to include numeric characters? \nYes or No');
-// console.log(numericCharacter);
-// var lowerCase = prompt('Would you like to include lower case letter? \nYes or No');
-// console.log(lowerCase);
-// var upperCase = prompt('would you like to include upper case letters? \nYes or No');
-// console.log(upperCase);
+// user input prompts
+var characterCount = prompt("How many characters would you like your password to be? \nMust be between 8 and 128 characters.");
+var specialCharacters = prompt('would you like to include special characters?');
+var numericCharacter = prompt('Would you like to include numeric characters?');
+var lowerCase = prompt('Would you like to include lower case letter?');
+var upperCase = prompt('would you like to include upper case letters?');
 
+console.log(characterCount);
+console.log(specialCharacters);
+console.log(numericCharacter);
+console.log(lowerCase);
+console.log(upperCase);
+
+// variables arrays to be used for random selection when creating password.
 var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseArray = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'L', 'K', 'J', 'H', 'G', 'F', 'D', 'S', 'A', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
 var numericArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialCharacterArray = ['?', '!', ',', '.', '<', '>', ';', ':', '/', '#', '@', '-', '_'];
+
+if ((characterCount !=== null) && (characterCount >= 8 || characterCount <= 128)){
+  var characterAmount = characterCount
+} else{
+  alert('invalid input. Character count must be between 8 and 128.')
+}
+
+
 
 
 // Assignment Code
